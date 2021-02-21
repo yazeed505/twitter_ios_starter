56192ef568bf12422e67d8dad6b224b9a64855cc
yazeed505 <54512668+yazeed505@users.noreply.github.com>
2021-02-21 12:58:11 -0600

    committing the code for tweet, retweet , and favoring

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -28,6 +28,10 @@ class HomeTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweet()
+    }
 
     func loadTweet(){
         
@@ -79,6 +83,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
     
